a48382aad2e25a12e8a88cf5b1e1b2c5bad44746
muzico <547153062@qq.com>
2024-12-13 10:23:16 +0800

    Create sunodata.js
    木子才歌曲精选数据源

---
+++ data/sunodata.js
@@ -0,0 +1,262 @@
+var tracks = [
+    {
+      "mp3": "https://cdn1.suno.ai/684af708-f5ab-4d40-a7c6-5df9da655e92.mp3",
+      "name2": "天空的颜色_20240708_92"
+    },
+    {
+      "mp3": "https://cdn1.suno.ai/bc58b48a-9cf4-4bf9-9d12-e80d5bcdd046.mp3",
+      "name2": "随笔04_20241210_46"
+    },
+    {
+      "mp3": "https://cdn1.suno.ai/f2fe8627-b375-4746-b4dd-6ee667e6c0e6.mp3",
+      "name2": "随笔04_20241210_e6"
+    },
+    {
+      "mp3": "https://cdn1.suno.ai/31021dcd-701e-455d-8efb-69ccd9d01650.mp3",
+      "name2": "她的故事_20240418_50"
+    },
+    {
+      "mp3": "https://cdn1.suno.ai/dace4b7a-654c-4a4b-9ea2-8c8dc7f4ed4c.mp3",
+      "name2": "她的故事（中间部分1分钟）_20240327_4c"
+    },
+    {
+      "mp3": "https://cdn1.suno.ai/97cffb99-0e8a-4eb8-ae35-1aec03c1d3cd.mp3",
+      "name2": "思念你_20240418_cd"
+    },
+    {
+      "mp3": "https://cdn1.suno.ai/d46ca8ec-aa77-43d3-ae45-7018807a148e.mp3",
+      "name2": "她的故事Lost_20240608_8e"
+    },
+    {
+      "mp3": "https://cdn1.suno.ai/1da2465c-21da-4cfe-bd95-c16d2ca1a890.mp3",
+      "name2": "会いたかった_20240510_90"
+    },
+    {
+      "mp3": "https://cdn1.suno.ai/bc56cd7c-da1f-46a0-af34-382820c3a736.mp3",
+      "name2": "她的故事_20240328_36"
+    },
+    {
+      "mp3": "https://cdn1.suno.ai/6cf5ff98-6d98-45e2-8ccc-7f196a1b2823.mp3",
+      "name2": "我和你_20240326_23"
+    },
+    {
+      "mp3": "https://cdn1.suno.ai/60af5cb5-aca3-4cae-bf3f-b592bda2e37b.mp3",
+      "name2": "彼女の物語は （腾讯翻译）_20240419_7b"
+    },
+    {
+      "mp3": "https://cdn1.suno.ai/45bb3244-ebc8-48e6-8597-3f4141d9d590.mp3",
+      "name2": "奇迹_20240418_90"
+    },
+    {
+      "mp3": "https://cdn1.suno.ai/6336ef87-03e4-4f15-be6b-c4ef294bd75b.mp3",
+      "name2": "思念你_20240418_5b"
+    },
+    {
+      "mp3": "https://cdn1.suno.ai/96781c78-b5b6-45b6-ada1-dd0f34003a87.mp3",
+      "name2": "黒い空_20240428_87"
+    },
+    {
+      "mp3": "https://cdn1.suno.ai/f64ce0db-5100-416e-b348-48291da9f221.mp3",
+      "name2": "黑色的天空_20240428_21"
+    },
+    {
+      "mp3": "https://cdn1.suno.ai/2863111e-ac97-4f7f-b532-86b5c41ff0f5.mp3",
+      "name2": "二人的物语_20240531_f5"
+    },
+    {
+      "mp3": "https://cdn1.suno.ai/09f78d66-fe99-4e92-b565-3a2f8d5db8c6.mp3",
+      "name2": "朦胧里_20240418_c6"
+    },
+    {
+      "mp3": "https://cdn1.suno.ai/34050eed-d22d-414a-866f-8dc0c1fc5334.mp3",
+      "name2": "思念你_20240418_34"
+    },
+    {
+      "mp3": "https://cdn1.suno.ai/645f7977-923d-4f02-a1df-2a26bae1234a.mp3",
+      "name2": "我和你Full_20240607_4a"
+    },
+    {
+      "mp3": "https://cdn1.suno.ai/a7500e41-d397-479e-9f5a-9ce03743c0e7.mp3",
+      "name2": "思念你_20240418_e7"
+    },
+    {
+      "mp3": "https://cdn1.suno.ai/27de49a2-2c58-49b3-bd64-ff92442ac018.mp3",
+      "name2": "奇迹_20240418_18"
+    },
+    {
+      "mp3": "https://cdn1.suno.ai/800d4c24-07bf-4bf5-a4cf-2ed21907fe20.mp3",
+      "name2": "只能够_20240520_20"
+    },
+    {
+      "mp3": "https://cdn1.suno.ai/15f66e54-5853-45ad-ba83-4a9d18c6502b.mp3",
+      "name2": "好想你_20240517_2b"
+    },
+    {
+      "mp3": "https://cdn1.suno.ai/7fa72244-0d85-4149-a354-b961fe6441fa.mp3",
+      "name2": "我和你_20240326_fa"
+    },
+    {
+      "mp3": "https://cdn1.suno.ai/09116e1b-564d-4387-9804-df52208fff23.mp3",
+      "name2": "我和你_20240603_23"
+    },
+    {
+      "mp3": "https://cdn1.suno.ai/61e92e8c-7149-4216-92d4-d8cda8b4bcd4.mp3",
+      "name2": "一辈子爱的人_20240502_d4"
+    },
+    {
+      "mp3": "https://cdn1.suno.ai/48c41a0a-9090-48e1-97a8-46f788da759a.mp3",
+      "name2": "奇迹_20240418_9a"
+    },
+    {
+      "mp3": "https://cdn1.suno.ai/99032033-73d8-4f89-9f9b-39e5015de6ae.mp3",
+      "name2": "黑色的天空_20240502_ae"
+    },
+    {
+      "mp3": "https://cdn1.suno.ai/28024927-2f25-4956-a17f-c57bb334de5a.mp3",
+      "name2": "我和你Full_20240607_5a"
+    },
+    {
+      "mp3": "https://cdn1.suno.ai/69e963df-c278-43b6-a841-0e823371fee1.mp3",
+      "name2": "她的故事_20240603_e1"
+    },
+    {
+      "mp3": "https://cdn1.suno.ai/47283f54-030b-4e2a-84ce-e22526662588.mp3",
+      "name2": "遥远的梦_20240514_88"
+    },
+    {
+      "mp3": "https://cdn1.suno.ai/0ea7e3ae-3a6f-4917-9898-c4fd42bb3a92.mp3",
+      "name2": "奇迹Full_20240609_92"
+    },
+    {
+      "mp3": "https://cdn1.suno.ai/ab5b7160-4564-4602-96cc-962fb29ba95e.mp3",
+      "name2": "我和你Full_20240607_5e"
+    },
+    {
+      "mp3": "https://cdn1.suno.ai/41ff1d34-3309-4736-9c83-f1ddc905829d.mp3",
+      "name2": "她的故事_20240418_9d"
+    },
+    {
+      "mp3": "https://cdn1.suno.ai/6e00ae5b-2c0f-4c14-a3e4-ecf7640b6f9f.mp3",
+      "name2": "漫溢的心，永远为你_20241024_9f"
+    },
+    {
+      "mp3": "https://cdn1.suno.ai/6814556c-c2f8-4e6c-9935-c7d9a0c9e49f.mp3",
+      "name2": "四月的谎言_20240628_9f"
+    },
+    {
+      "mp3": "https://cdn1.suno.ai/915c46fd-b36b-4aff-984f-0517e33286af.mp3",
+      "name2": "随笔01_20240515_af"
+    },
+    {
+      "mp3": "https://cdn1.suno.ai/363b6d75-b4f4-4193-b949-61d623ebcd37.mp3",
+      "name2": "少年Other_20241113_37"
+    },
+    {
+      "mp3": "https://cdn1.suno.ai/6a20412a-d80a-4de9-b3b8-d7cc2ece82af.mp3",
+      "name2": "一万年_20241115_af"
+    },
+    {
+      "mp3": "https://cdn1.suno.ai/6d2f9b20-013c-45ac-aa1e-a0594ce2a1af.mp3",
+      "name2": "随笔03_20241118_af"
+    },
+    {
+      "mp3": "https://cdn1.suno.ai/bfb0a5d2-4903-40a6-91c0-288edb54329a.mp3",
+      "name2": "随笔03_20241118_9a"
+    },
+    {
+      "mp3": "https://cdn1.suno.ai/b5534507-c3e8-4829-ab27-85e7161a27b9.mp3",
+      "name2": "世界初见的奇迹，二人的物语_20241025_b9"
+    },
+    {
+      "mp3": "https://cdn1.suno.ai/114ce8da-c599-4cd2-95bd-35879382a49c.mp3",
+      "name2": "星空_20240418_9c"
+    },
+    {
+      "mp3": "https://cdn1.suno.ai/65e989df-1234-40b0-b2a7-628bc7021736.mp3",
+      "name2": "四月的谎言，遥远的梦_20241104_36"
+    },
+    {
+      "mp3": "https://cdn1.suno.ai/e0d6738a-39dd-4b93-be36-6582e9a7ef77.mp3",
+      "name2": "黑色的天空_20240428_77"
+    },
+    {
+      "mp3": "https://cdn1.suno.ai/e8784da5-83ae-4003-92d6-7963c4d79100.mp3",
+      "name2": "二人的物语_20240628_00"
+    },
+    {
+      "mp3": "https://cdn1.suno.ai/f0c200fe-5526-4fbb-ac8b-19cb1be753bc.mp3",
+      "name2": "我们的故事_20240615_bc"
+    },
+    {
+      "mp3": "https://cdn1.suno.ai/74ff1981-0912-49ba-a38e-949d418bdde9.mp3",
+      "name2": "只能够_20240627_e9"
+    },
+    {
+      "mp3": "https://cdn1.suno.ai/69801f27-a677-4e1c-accb-34971c883571.mp3",
+      "name2": "世界初见的奇迹，二人的物语_20241025_71"
+    },
+    {
+      "mp3": "https://cdn1.suno.ai/4cac6d17-6f45-4cb5-be41-8347de92d37f.mp3",
+      "name2": "随笔02_20241102_7f"
+    },
+    {
+      "mp3": "https://cdn1.suno.ai/01ffb014-c959-48d7-9918-be5b5a470094.mp3",
+      "name2": "随笔03_20241118_94"
+    },
+    {
+      "mp3": "https://cdn1.suno.ai/b02210f3-4d9f-4531-8a95-43fa9ccc047c.mp3",
+      "name2": "我和你Full_20240607_7c"
+    },
+    {
+      "mp3": "https://cdn1.suno.ai/f2fb0ca1-38e1-4b08-a0e8-cefdd30dedea.mp3",
+      "name2": "她的故事Most_20240608_ea"
+    },
+    {
+      "mp3": "https://cdn1.suno.ai/7b2493ab-29e0-4090-b318-f9edccfe3e85.mp3",
+      "name2": "好想你_20240510_85"
+    },
+    {
+      "mp3": "https://cdn1.suno.ai/adcaf958-39c1-4ce5-b57f-cfb23623ebcc.mp3",
+      "name2": "她的故事_20240326_cc"
+    },
+    {
+      "mp3": "https://cdn1.suno.ai/c019d825-8644-4877-b091-260d9cfbf4be.mp3",
+      "name2": "二人的物语_20240613_be"
+    },
+    {
+      "mp3": "https://cdn1.suno.ai/2a618aab-641e-4516-b5de-04fee44a844a.mp3",
+      "name2": "好想你_20240628_4a"
+    },
+    {
+      "mp3": "https://cdn1.suno.ai/b291b851-2b75-41a8-8bdc-7b5fc23f8213.mp3",
+      "name2": "只能够_20240418_13"
+    },
+    {
+      "mp3": "https://cdn1.suno.ai/8ce44cf9-007e-45bd-951e-91bd4cb3b7df.mp3",
+      "name2": "世界が初めて見た奇跡、二人の物語_20241026_df"
+    },
+    {
+      "mp3": "https://cdn1.suno.ai/0876a883-5533-42a1-9d85-d4baf9693a6b.mp3",
+      "name2": "少年Other_20241113_6b"
+    },
+    {
+      "mp3": "https://cdn1.suno.ai/804553f9-1a5a-4c7a-81bc-2a8c1b44ac1c.mp3",
+      "name2": "四月的谎言_20240520_1c"
+    },
+    {
+      "mp3": "https://cdn1.suno.ai/99d8c7ec-4a94-4475-93a9-2641658ade80.mp3",
+      "name2": "天空的颜色_20240708_80"
+    },
+    {
+      "mp3": "https://cdn1.suno.ai/27d1b168-6d77-44e0-a7d8-003bd4f6903e.mp3",
+      "name2": "初见的奇迹other_20241110_3e"
+    },
+    {
+      "mp3": "https://cdn1.suno.ai/e8c06ecf-c08e-40bc-a18f-0642344969d3.mp3",
+      "name2": "初见的奇迹other_20241110_d3"
+    },
+    {
+      "mp3": "https://cdn1.suno.ai/51a10a40-a14f-49e2-a3e5-d77fd193d684.mp3",
+      "name2": "初见的奇迹other_20241110_84"
+    }
+  ];

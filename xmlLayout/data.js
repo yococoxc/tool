8e6240f14b2aea6b09c735f328cc3576a85040f1
muzico <547153062@qq.com>
2025-09-28 08:51:38 +0800

    Update data.js
    更新数据

--- xmlLayout/data.js
+++ xmlLayout/data.js
@@ -3,13 +3,13 @@ window.categoriesData = [
     {
         "name": "项目A",
         "items": [
-            {"name":"主页","pic":"","xml":"a00.xml"},
-            {"name":"首页","pic":"","xml":"a01.xml"},
-            {"name":"延时","pic":"","xml":"a02.xml"},
-            {"name":"通道","pic":"","xml":"a03.xml"},
-            {"name":"EQ","pic":"","xml":"a04.xml"},
-            {"name":"系统","pic":"","xml":"a05.xml"},
-            {"name":"EQ item","pic":"","xml":"a0400.xml"},
+            {"name":"主页","pic":"","xml":"./res/a00.xml"},
+            {"name":"首页","pic":"","xml":"./res/a01.xml"},
+            {"name":"延时","pic":"","xml":"./res/a02.xml"},
+            {"name":"通道","pic":"","xml":"./res/a03.xml"},
+            {"name":"EQ","pic":"","xml":"./res/a04.xml"},
+            {"name":"系统","pic":"","xml":"./res/a05.xml"},
+            {"name":"EQ item","pic":"","xml":"./res/a0400.xml"},
         ]
     },
 ];

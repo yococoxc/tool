5ec57620b1834182420994e994c78f1fb111bf0d
muzico <547153062@qq.com>
2025-09-28 08:49:27 +0800

    Create data.js
    添加xmlLayout数据。

---
+++ xmlLayout/data.js
@@ -0,0 +1,16 @@
+// 分类数据 - 以JavaScript对象形式存储
+window.categoriesData = [
+    {
+        "name": "项目A",
+        "items": [
+            {"name":"主页","pic":"","xml":"a00.xml"},
+            {"name":"首页","pic":"","xml":"a01.xml"},
+            {"name":"延时","pic":"","xml":"a02.xml"},
+            {"name":"通道","pic":"","xml":"a03.xml"},
+            {"name":"EQ","pic":"","xml":"a04.xml"},
+            {"name":"系统","pic":"","xml":"a05.xml"},
+            {"name":"EQ item","pic":"","xml":"a0400.xml"},
+        ]
+    },
+];
+    

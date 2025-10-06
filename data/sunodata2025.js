9966e7ed69be714aeaa06c26848e672954b0ca41
muzico <547153062@qq.com>
2025-10-06 10:28:03 +0800

    Create sunodata2025.js
    2025年演唱会歌曲编排

---
+++ data/sunodata2025.js
@@ -0,0 +1,470 @@
+var tracks2025 = [
+    {
+        title: "思念你",
+        array: [
+              {
+                "mp3": "https://cdn1.suno.ai/a7500e41-d397-479e-9f5a-9ce03743c0e7.mp3",
+                "name2": "思念你_20240418_e7"
+              },
+        ]
+    },
+    {
+        title: "少年Other",
+        array: [
+            {
+                "mp3": "https://cdn1.suno.ai/0876a883-5533-42a1-9d85-d4baf9693a6b.mp3",
+                "name2": "少年Other_20241113_6b"
+            },
+        ]
+    },
+    {
+        title: "她的故事",
+        array: [
+              {
+                "mp3": "https://cdn1.suno.ai/69e963df-c278-43b6-a841-0e823371fee1.mp3",
+                "name2": "她的故事_20240603_e1"
+              },
+        ]
+    },
+    {
+        title: "黑色的天空",
+        array: [
+              {
+                "mp3": "https://cdn1.suno.ai/99032033-73d8-4f89-9f9b-39e5015de6ae.mp3",
+                "name2": "黑色的天空_20240502_ae"
+              },
+        ]
+    },
+    {
+        title: "星空",
+        array: [
+            {
+                "mp3": "https://cdn1.suno.ai/114ce8da-c599-4cd2-95bd-35879382a49c.mp3",
+                "name2": "星空_20240418_9c"
+            }
+        ]
+    },
+    {
+        title: "我们的故事",
+        array: [
+            {
+              "mp3": "https://cdn1.suno.ai/f0c200fe-5526-4fbb-ac8b-19cb1be753bc.mp3",
+              "name2": "我们的故事_20240615_bc"
+            },
+        ]
+    },
+    {
+        title: "奇迹",
+        array: [
+              {
+                "mp3": "https://cdn1.suno.ai/0ea7e3ae-3a6f-4917-9898-c4fd42bb3a92.mp3",
+                "name2": "奇迹Full_20240609_92"
+              },
+        ]
+    },
+    {
+        title: "初见的奇迹other",
+        array: [
+              {
+                "mp3": "https://cdn1.suno.ai/51a10a40-a14f-49e2-a3e5-d77fd193d684.mp3",
+                "name2": "初见的奇迹other_20241110_84"
+              }
+        ]
+    },
+    {
+        title: "世界初见的奇迹",
+        array: [
+            {
+                "mp3": "https://cdn1.suno.ai/6e00ae5b-2c0f-4c14-a3e4-ecf7640b6f9f.mp3",
+                "name2": "世界初见的奇迹_20241024_9f"
+              },
+        ]
+    },
+    {
+        title: "我和你",
+        array: [
+              {
+                "mp3": "https://cdn1.suno.ai/ab5b7160-4564-4602-96cc-962fb29ba95e.mp3",
+                "name2": "我和你Full_20240607_5e"
+              },
+        ]
+    },
+    {
+      title: "随笔12-白色的世界",
+      array: [
+        {
+          "mp3": "https://cdn1.suno.ai/e1cd6a82-ea00-4047-a35a-7e2b0dafdf3d.mp3",
+          "name2": "随笔12-白色的世界_3d"
+        }
+      ]
+    },
+    {
+        title: "天空的颜色",
+        array: [
+            {
+                "mp3": "https://cdn1.suno.ai/99d8c7ec-4a94-4475-93a9-2641658ade80.mp3",
+                "name2": "天空的颜色_20240708_80"
+            }
+        ]
+    },
+    {
+        title: "二人的物语",
+        array: [
+            {
+                "mp3": "https://cdn1.suno.ai/e8784da5-83ae-4003-92d6-7963c4d79100.mp3",
+                "name2": "二人的物语_20240628_00"
+              },
+        ]
+    },
+    {
+        title: "一辈子爱的人",
+        array: [
+            {
+                "mp3": "https://cdn1.suno.ai/61e92e8c-7149-4216-92d4-d8cda8b4bcd4.mp3",
+                "name2": "一辈子爱的人_20240502_d4"
+            }
+        ]
+    },
+    {
+        title: "遥远的梦",
+        array: [
+            {
+                "mp3": "https://cdn1.suno.ai/47283f54-030b-4e2a-84ce-e22526662588.mp3",
+                "name2": "遥远的梦_20240514_88"
+              },
+        ]
+    },
+    {
+        title: "只能够",
+        array: [
+              {
+                "mp3": "https://cdn1.suno.ai/74ff1981-0912-49ba-a38e-949d418bdde9.mp3",
+                "name2": "只能够_20240627_e9"
+              },
+        ]
+    },
+    {
+      title: "天空中只看见一颗星",
+      array: [
+        {
+          "mp3": "https://cdn1.suno.ai/34d8dd75-5cb3-4669-b718-461fb1361742.mp3",
+          "name2": "天空中只看见一颗星_42"
+        },
+      ]
+    },
+    {
+        title: "四月的谎言",
+        array: [
+            {
+                "mp3": "https://cdn1.suno.ai/6814556c-c2f8-4e6c-9935-c7d9a0c9e49f.mp3",
+                "name2": "四月的谎言_20240628_9f"
+              },
+        ]
+    },
+    {
+        title: "随笔04-眼泪的味道",
+        array: [
+            {
+                "mp3": "https://cdn1.suno.ai/bc58b48a-9cf4-4bf9-9d12-e80d5bcdd046.mp3",
+                "name2": "随笔04-眼泪的味道_20241210_46"
+            },
+        ]
+    },
+    {
+        title: "好想你",
+        array: [
+              {
+                "mp3": "https://cdn1.suno.ai/2a618aab-641e-4516-b5de-04fee44a844a.mp3",
+                "name2": "好想你_20240628_4a"
+              },
+        ]
+    },
+    {
+        title: "放下的梦",
+        array: [
+          {
+            "mp3": "https://cdn1.suno.ai/8dd5ac55-4658-4b8a-a8f7-88e77fbdac8b.mp3",
+            "name2": "放下的梦_20250103_8b"
+          },
+        ]
+    },
+    {
+      title: "随笔21-信",
+      array: [
+        {
+          "mp3": "https://cdn1.suno.ai/9f4a7728-3871-4615-b2dc-44b828041100.mp3",
+          "name2": "随笔21-信_00"
+        },
+      ]
+    },
+    {
+      title: "随笔27-如果我还爱你",
+      array: [
+        {
+          "mp3": "https://cdn1.suno.ai/71b3c537-b777-4ee7-a58e-6620dd9e391e.mp3",
+          "name2": "随笔27-如果我还爱你_1e"
+        },
+      ]
+    },
+    {
+      title: "随笔11-坦诚",
+      array: [
+        {
+          "mp3": "https://cdn1.suno.ai/79232f93-b291-45f9-89cb-2d643c816847.mp3",
+          "name2": "随笔11-坦诚_47"
+        }
+      ]
+    },
+    {
+      title: "随笔19-四月的思念",
+      array: [
+        {
+          "mp3": "https://cdn1.suno.ai/118e9b41-3f84-4c9d-836b-5af51c4d3406.mp3",
+          "name2": "随笔19-四月的思念_06"
+        }
+      ]
+    },
+    {
+      title: "随笔23-四月的梦",
+      array: [
+        {
+          "mp3": "https://cdn1.suno.ai/4b1ee941-dd24-49d0-ba4d-3442b35687f9.mp3",
+          "name2": "随笔23-四月的梦_f9"
+        },
+      ]
+    },
+    {
+      title: "随笔24-五月的歌",
+      array: [
+        {
+          "mp3": "https://cdn1.suno.ai/04a27349-98b5-4a1f-b02c-72d9aae3b7e2.mp3",
+          "name2": "随笔24-五月的歌_e2"
+        }
+      ]
+    },
+    {
+      title: "随笔32-雨",
+      array: [
+        {
+          "mp3": "https://cdn1.suno.ai/5ebb89cb-d9d9-4afc-9cfd-c9bffe8240b3.mp3",
+          "name2": "随笔32-雨_b3"
+        },
+      ]
+    },
+    {
+      title: "随笔33-无法细说",
+      array: [
+        {
+          "mp3": "https://cdn1.suno.ai/c8f6fafe-46fb-4b9d-8902-5ac15871d551.mp3",
+          "name2": "随笔33-无法细说_51"
+        },
+      ]
+    },
+    {
+        title: "一万年",
+        array: [
+            {
+                "mp3": "https://cdn1.suno.ai/6a20412a-d80a-4de9-b3b8-d7cc2ece82af.mp3",
+                "name2": "一万年_20241115_af"
+            }
+        ]
+    },
+    {
+      title: "随笔15-踏出第一步",
+      array: [
+        {
+          "mp3": "https://cdn1.suno.ai/cdd0f88b-392a-47f8-a07f-b9db73ec04da.mp3",
+          "name2": "随笔15-踏出第一步_da"
+        }
+      ]
+    },
+    {
+        title: "随笔05-过去",
+        array: [
+          {
+            "mp3": "https://cdn1.suno.ai/f09f41bd-6eba-4ee3-8458-d70a2cdd0609.mp3",
+            "name2": "随笔05-过去_20250102_09"
+          },
+        ]
+    },
+    {
+      title: "随笔09-江郎才尽",
+      array: [
+        {
+          "mp3": "https://cdn1.suno.ai/5849f251-62a4-4580-a479-b0c8b9743126.mp3",
+          "name2": "随笔09-江郎才尽_26"
+        }
+      ]
+    },
+    {
+      title: "随笔30-伪物",
+      array: [
+        {
+          "mp3": "https://cdn1.suno.ai/0460de83-88fd-4f9e-8311-13c2ccebf27b.mp3",
+          "name2": "随笔30-伪物_52"
+        },
+      ]
+    },
+    {
+      title: "随笔30-真物",
+      array: [
+        {
+          "mp3": "https://cdn1.suno.ai/48831a91-64ca-4bd5-b3da-8ec86e332b42.mp3",
+          "name2": "随笔30-真物_42"
+        }
+      ]
+    },
+    {
+      title: "随笔31-碎片",
+      array: [
+        {
+          "mp3": "https://cdn1.suno.ai/a29be176-2e4b-40c6-a9c9-a300d8ac1662.mp3",
+          "name2": "随笔31-碎片_62"
+        },
+      ]
+    },
+    {
+      title: "随笔25-咖啡",
+      array: [
+        {
+          "mp3": "https://cdn1.suno.ai/35f73b7d-a1c2-414a-b6c8-067178d67384.mp3",
+          "name2": "随笔25-咖啡_84"
+        },
+      ]
+    },
+    {
+      title: "随笔29-再见",
+      array: [
+        {
+          "mp3": "https://cdn1.suno.ai/af314a58-3cfc-440b-a58a-8a47d279ae2a.mp3",
+          "name2": "随笔29-再见_2a"
+        },
+      ]
+    },
+    {
+        title: "圣诞节",
+        array: [
+          {
+            "mp3": "https://cdn1.suno.ai/70abc2e1-8b3c-494c-9444-694d452a6197.mp3",
+            "name2": "圣诞节_20241226_97"
+          },
+        ]
+    },
+    {
+        title: "随笔02-爱你",
+        array: [
+            {
+                "mp3": "https://cdn1.suno.ai/4cac6d17-6f45-4cb5-be41-8347de92d37f.mp3",
+                "name2": "随笔02-爱你_20241102_7f"
+            }
+        ]
+    },
+    {
+        title: "我愿意",
+        array: [
+          {
+            "mp3": "https://cdn1.suno.ai/b905ef9d-065b-4439-b5fd-6b74b0f99dca.mp3",
+            "name2": "我愿意_20241230_ca"
+          },
+        ]
+    },
+    {
+      title: "随笔34-回到过去",
+      array: [
+        {
+          "mp3": "https://cdn1.suno.ai/37c310d6-a946-442a-9676-a52cacd17ec6.mp3",
+          "name2": "随笔34-回到过去_c6"
+        },
+      ]
+    },
+    {
+      title: "随笔26-和你",
+      array: [
+        {
+          "mp3": "https://cdn1.suno.ai/01e16bca-8b7f-4922-951e-a7021b1be0b1.mp3",
+          "name2": "随笔26-和你_b1"
+        },
+      ]
+    },
+    {
+      title: "随笔17-爱的告白",
+      array: [
+        {
+          "mp3": "https://cdn1.suno.ai/b577ce1b-67fb-4aff-853a-aaa8c64f79ab.mp3",
+          "name2": "随笔17-爱的告白_ab"
+        },
+      ]
+    },
+    {
+      title: "随笔18-我的全部",
+      array: [
+        {
+          "mp3": "https://cdn1.suno.ai/9c263698-1b3a-4295-a5f0-1d320fef272a.mp3",
+          "name2": "随笔18-我的全部_2a"
+        },
+      ]
+    },
+    {
+      title: "随笔07:伽罗爱敏",
+      array: [
+        {
+          "mp3": "https://cdn1.suno.ai/91c96b53-878b-457a-b955-d46063a12a48.mp3",
+          "name2": "随笔07:伽罗爱敏_48"
+        },
+      ]
+    },
+    {
+      title: "随笔22-哦润吉",
+      array: [
+        {
+          "mp3": "https://cdn1.suno.ai/752cb0e2-5c64-402c-8b3c-f6765094373a.mp3",
+          "name2": "随笔22-哦润吉_3a"
+        },
+      ]
+    },
+    {
+        title: "随笔03-努力",
+        array: [
+            {
+                "mp3": "https://cdn1.suno.ai/01ffb014-c959-48d7-9918-be5b5a470094.mp3",
+                "name2": "随笔03-努力_20241118_94"
+            },
+        ]
+    },
+    {
+      title: "随笔28-受伤的总是我",
+      array: [
+        {
+          "mp3": "https://cdn1.suno.ai/0e60351b-10a1-4820-8039-c1e459a02225.mp3",
+          "name2": "随笔28-受伤的总是我_25"
+        }
+      ]
+    },
+    {
+      title: "随笔06-黑暗时刻，我的奋斗",
+      array: [
+        {
+          "mp3": "https://cdn1.suno.ai/45e4183f-977b-4cf6-8650-9b5101ca265c.mp3",
+          "name2": "随笔06-黑暗时刻，我的奋斗_5c"
+        },
+      ]
+    },
+    {
+        title: "四月的谎言，遥远的梦",
+        array: [
+            {
+                "mp3": "https://cdn1.suno.ai/65e989df-1234-40b0-b2a7-628bc7021736.mp3",
+                "name2": "四月的谎言，遥远的梦_20241104_36"
+            }
+        ]
+    },
+    {
+        title: "世界初见的奇迹，二人的物语",
+        array: [
+            {
+                "mp3": "https://cdn1.suno.ai/b5534507-c3e8-4829-ab27-85e7161a27b9.mp3",
+                "name2": "世界初见的奇迹，二人的物语_20241025_b9"
+            },
+        ]
+    },
+];

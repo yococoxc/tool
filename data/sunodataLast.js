bad0255951f6f69c98eb1bc5cb82b9116eba99d6
muzico <547153062@qq.com>
2026-04-01 16:54:24 +0800

    Create sunodataLast.js
    创建最新歌曲列表

---
+++ data/sunodataLast.js
@@ -0,0 +1,19 @@
+var tracksLast = [
+    {
+        title: "串烧随笔01",
+        array: [
+              {
+                "mp3": "https://cdn1.suno.ai/85d8c79a-6cd6-4ea8-93ff-15acc1e9c0ec.mp3",
+                "name2": "串烧随笔01_ec"
+              },
+              {
+                "mp3": "https://cdn1.suno.ai/9542d537-5db2-44b6-ae34-0df16d6084fb.mp3",
+                "name2": "串烧随笔01_fb"
+              },
+              {
+                "mp3": "https://cdn1.suno.ai/3bb20644-1753-4dc4-a82b-fb383bb4d15c.mp3",
+                "name2": "串烧随笔01_5c"
+              },
+        ]
+    },
+];

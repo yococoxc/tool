9f37fcd12cb422f0f3ec0fa215a0ea3afb0aa887
muzico <547153062@qq.com>
2026-04-22 15:46:11 +0800

    Update sunodataLast.js
    更新最新歌曲的内容

--- data/sunodataLast.js
+++ data/sunodataLast.js
@@ -1,5 +1,49 @@
 var tracksLast = [
     {
+        title: "随笔43-四月的春天",
+        array: [
+              {
+                "mp3": "https://cdn1.suno.ai/a03dc86d-7963-41ea-9fcd-a69d9208e2d7.mp3",
+                "name2": "随笔43-四月的春天_d7"
+              },
+        ]
+    },
+    {
+        title: "心中有段情",
+        array: [
+              {
+                "mp3": "https://cdn1.suno.ai/0e05089c-482c-4bb2-9e5a-04ea5d60e4cc.mp3",
+                "name2": "心中有段情_cc"
+              },
+        ]
+    },
+    {
+        title: "随笔41-融雪",
+        array: [
+              {
+                "mp3": "https://cdn1.suno.ai/8011e1d8-5514-4d4a-83b0-981ba997861a.mp3",
+                "name2": "随笔41-融雪_1a"
+              },
+        ]
+    },
+    {
+        title: "串烧随笔01",
+        array: [
+              {
+                "mp3": "https://cdn1.suno.ai/85d8c79a-6cd6-4ea8-93ff-15acc1e9c0ec.mp3",
+                "name2": "串烧随笔01_ec"
+              },
+              {
+                "mp3": "https://cdn1.suno.ai/9542d537-5db2-44b6-ae34-0df16d6084fb.mp3",
+                "name2": "串烧随笔01_fb"
+              },
+              {
+                "mp3": "https://cdn1.suno.ai/3bb20644-1753-4dc4-a82b-fb383bb4d15c.mp3",
+                "name2": "串烧随笔01_5c"
+              },
+        ]
+    },
+    {
         title: "串烧随笔01",
         array: [
               {

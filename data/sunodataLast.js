8540d7e67af706126ecdffa81241c7964ac6a341
muzico <547153062@qq.com>
2026-05-11 16:22:44 +0800

    Update sunodataLast.js
    更新随笔44的数据内容

--- data/sunodataLast.js
+++ data/sunodataLast.js
@@ -1,5 +1,22 @@
 var tracksLast = [
     {
+        title: "随笔44-仰望夜空",
+        array: [
+              {
+                "mp3": "https://cdn1.suno.ai/7f4410c5-ce06-434c-9f31-2bd2d6e4b3a0.mp3",
+                "name2": "随笔44-仰望夜空_a0"
+              },
+              {
+                "mp3": "https://cdn1.suno.ai/0611e584-d275-4cf4-8ad8-96a4ca3d44c3.mp3",
+                "name2": "随笔44-仰望夜空_c3"
+              },
+              {
+                "mp3": "https://cdn1.suno.ai/182eea1a-37db-4f07-9c32-16221a674df7.mp3",
+                "name2": "随笔44-仰望夜空_f7"
+              },
+        ]
+    },
+    {
         title: "随笔43-四月的春天",
         array: [
               {
@@ -23,23 +40,6 @@ var tracksLast = [
               {
                 "mp3": "https://cdn1.suno.ai/8011e1d8-5514-4d4a-83b0-981ba997861a.mp3",
                 "name2": "随笔41-融雪_1a"
-              },
-        ]
-    },
-    {
-        title: "串烧随笔01",
-        array: [
-              {
-                "mp3": "https://cdn1.suno.ai/85d8c79a-6cd6-4ea8-93ff-15acc1e9c0ec.mp3",
-                "name2": "串烧随笔01_ec"
-              },
-              {
-                "mp3": "https://cdn1.suno.ai/9542d537-5db2-44b6-ae34-0df16d6084fb.mp3",
-                "name2": "串烧随笔01_fb"
-              },
-              {
-                "mp3": "https://cdn1.suno.ai/3bb20644-1753-4dc4-a82b-fb383bb4d15c.mp3",
-                "name2": "串烧随笔01_5c"
               },
         ]
     },
